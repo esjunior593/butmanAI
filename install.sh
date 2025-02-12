fddc82d262ce3144c5ffec4906c86a4d506acb8d
esjunior593 <esjunior593@gmail.com>
2025-02-11 22:57:38 -0500

    Eliminando node_modules del seguimiento3

--- install.sh
+++ install.sh
@@ -13,6 +13,24 @@ apt-get update && apt-get install -y \
     xdg-utils \
     fonts-liberation \
     libgbm1 \
-    libX11-xcb1  # <-- Librería faltante
+    libX11-xcb1 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxext6 \
+    libxfixes3 \
+    libcups2 \
+    libdbus-1-3 \
+    libfontconfig1 \
+    libxrandr2 \
+    libxrender1 \
+    libgtk-3-0 \
+    libpangocairo-1.0-0 \
+    libcairo2 \
+    libpango-1.0-0 \
+    libatk1.0-0 \
+    libgdk-pixbuf2.0-0 \
+    libgdk-pixbuf-2.0-0 \
+    libglib2.0-0
 
 echo "✅ Dependencias instaladas correctamente."
